0f35979901aff2cff381a388d566f529598d3317
ehsanmavaei <ehsanmavaei@gmail.com>
2022-04-11 15:12:01 +0430

    finish the mobile view

--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,21 @@ console.log('HELLO');
 const test = () => {
 	console.log('this is a test');
 };
+ 
+
+const open_share = document.querySelector('.open-share');
+const button = document.querySelector('.arrow');
+const arrow = document.querySelector('#arrow');
+
+button.addEventListener('click', event => {
+	console.log("id");
+	open_share.classList.add('active')
+	
+ 
+});
+arrow.addEventListener('click', event => {
+	console.log("id");
+	open_share.classList.remove('active')
+	
+ 
+});
